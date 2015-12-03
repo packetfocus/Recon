ac7679fcac7274d09cd725a4909773decc20803a
Joshua Perrymon <packetfocus@users.noreply.github.com>
2015-12-03 15:08:46 -0600

    Update recon.sh
    Added command line options for output file and directory

--- recon.sh
+++ recon.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 if [ "$1" = "" ]; then
-   echo USAGE: "./recon cisco.com"
-   echo "./scan DOMAIN"
+   echo USAGE: "./scan cisco.com /tmp/output.txt /tmp/output/    "
+   echo "./scan DOMAIN OUTPUTFILE OUTPUTDIRECTORY"
    exit
 fi
 
 echo "Starting SubDomain Brute Force!"
 echo "testing $1"
-python /tools/subbrute/subbrute.py $1 > /tmp/output.txt 
+echo "output file for subbrute $2"
+echo "output directory for EyeWitness $3"
+python /tools/subbrute/subbrute.py $1 > $2 
 echo "Taking SubDomain list and running it through EyeWitness"
-python /tools/EyeWitness/EyeWitness.py -f /tmp/output/output.txt --web --results 250 -d /tmp/output/ --no-prompt
-echo 'DONE! Check /tmp/output/report.html for output'
+python /tools/EyeWitness/EyeWitness.py -f $2 --web --results 250 -d $3 --no-prompt
+echo 'DONE! Check $3 report.html for output'
