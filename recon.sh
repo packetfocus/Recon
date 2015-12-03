68043f83f3542a3a1dde67eea0fc961c643b874c
Joshua Perrymon <packetfocus@users.noreply.github.com>
2015-12-03 17:11:49 -0600

    Update recon.sh
    create output file and not append

--- recon.sh
+++ recon.sh
@@ -12,7 +12,7 @@ echo "OPTIONS------"
 echo "Output file for SubBrute $2"
 echo "Output directory for EyeWitness $3"
 echo "Starting Sub-Domain brute force. be PATIENT, this will take some time"
-python ~/tools/subbrute/subbrute.py $1 >> $2 
+python ~/tools/subbrute/subbrute.py $1 > $2 
 echo "Taking SubDomain list and running it through EyeWitness"
 python ~/tools/EyeWitness/EyeWitness.py -f $2 --web --results 250 -d $3 --no-prompt
 echo 'DONE! Check $3 report.html for output'
