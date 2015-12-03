37b5c05175da84878745665bafab637bf72fcb8d
Joshua Perrymon <packetfocus@users.noreply.github.com>
2015-12-03 15:47:05 -0600

    Create setup.sh
    Basic setup that creates Tools directory and installs SubBrute and EyeWitness

---
+++ setup.sh
@@ -0,0 +1,30 @@
+echo "checking to see if /tools directory exists"
+
+if [ ! -d ../tools ]
+then
+    mkdir -p /tools
+else
+    echo "TOOLS Directory exists"
+fi
+echo "Checking to see if EyeWitness is installed"
+
+if [ ! -d ../tools/EyeWitness/ ]
+then
+    cd ../tools
+    echo "Downloading and Installing EyeWitness"
+    git clone https://github.com/ChrisTruncer/EyeWitness.git
+else
+    echo "EyeWitnedd is installed"
+fi
+
+echo "Checking to see if SubBrute is installed"
+
+if [ ! -d ../tools/subbrute/ ]
+then
+    cd ../tools
+    echo "Downloading and Installing SubBrute"
+    git clone https://github.com/TheRook/subbrute.git
+else
+    echo "SubBrute is Already Installed"
+
+fi
