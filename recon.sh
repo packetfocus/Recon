dfb1bbabb7e07e988502cba7d39693a00859c8d1
Joshua Perrymon <packetfocus@users.noreply.github.com>
2015-12-03 15:58:48 -0600

    Update recon.sh
    updated path to tools using ~

--- recon.sh
+++ recon.sh
@@ -10,7 +10,7 @@ echo "Starting SubDomain Brute Force!"
 echo "testing $1"
 echo "output file for subbrute $2"
 echo "output directory for EyeWitness $3"
-python /tools/subbrute/subbrute.py $1 > $2 
+python ~/tools/subbrute/subbrute.py $1 > $2 
 echo "Taking SubDomain list and running it through EyeWitness"
-python /tools/EyeWitness/EyeWitness.py -f $2 --web --results 250 -d $3 --no-prompt
+python ~/tools/EyeWitness/EyeWitness.py -f $2 --web --results 250 -d $3 --no-prompt
 echo 'DONE! Check $3 report.html for output'
